b8ff6566c3038d56aaf5799cbf42a38a354295f3
Andreas Schlappig <github@lominart.net>
2012-03-09 22:20:42 +0100

    Zweite Zeile in Klasse Main. Placebo-Klasse ergänzt.

--- TestProject2/src/de/schlappig/gittest/Main.java
+++ TestProject2/src/de/schlappig/gittest/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Test");
+		System.out.println("Zeile 2");
 	}
 
 }
---
+++ TestProject2/src/de/schlappig/gittest/Placebo.java
@@ -0,0 +1,5 @@
+package de.schlappig.gittest;
+
+public class Placebo {
+
+}
